3deb6fbbf1cad992f4523df77bfd4f3897d83a5b
XXXJay.C <jay6677884@gmail.com>
2023-10-30 10:08:26 +0800

    Upate:

--- src/main/java/com/example/demofakecloud/entity/Impl/CustomUserDetails.java
+++ src/main/java/com/example/demofakecloud/entity/Impl/CustomUserDetails.java
@@ -23,7 +23,7 @@ public class CustomUserDetails implements UserDetails {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return user.getRoles().stream()
+    return  user.getRoles().stream()
         .map(role -> new SimpleGrantedAuthority("ROLE_" + role.getName()))
         .collect(Collectors.toList());
   }
